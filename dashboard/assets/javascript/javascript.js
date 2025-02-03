53890a202f03190ef4f2c0784ee27894e01723bc
sulaiman-khan <sallokhan01999@gmail.com>
2025-02-03 21:18:26 +0500

    first commit

---
+++ dashboard/assets/javascript/javascript.js
@@ -0,0 +1,140 @@
+var sync1 = $(".slider");
+var sync2 = $(".navigation-thumbs");
+
+var thumbnailItemClass = '.owl-item';
+
+var slides = sync1.owlCarousel({
+	video:true,
+  startPosition: 12,
+  items:1,
+  loop:true,
+  margin:10,
+  autoplay:true,
+  autoplayTimeout:6000,
+  autoplayHoverPause:false,
+  nav: false,
+  dots: false,
+  
+}).on('changed.owl.carousel', syncPosition);
+
+function syncPosition(el) {
+  $owl_slider = $(this).data('owl.carousel');
+  var loop = $owl_slider.options.loop;
+
+  if(loop){
+    var count = el.item.count-1;
+    var current = Math.round(el.item.index - (el.item.count/2) - .5);
+    if(current < 0) {
+        current = count;
+    }
+    if(current > count) {
+        current = 0;
+    }
+  }else{
+    var current = el.item.index;
+  }
+
+  var owl_thumbnail = sync2.data('owl.carousel');
+  var itemClass = "." + owl_thumbnail.options.itemClass;
+
+
+  var thumbnailCurrentItem = sync2
+  .find(itemClass)
+  .removeClass("synced")
+  .eq(current);
+
+  thumbnailCurrentItem.addClass('synced');
+
+  if (!thumbnailCurrentItem.hasClass('active')) {
+    var duration = 300;
+    sync2.trigger('to.owl.carousel',[current, duration, true]);
+  }   
+}
+var thumbs = sync2.owlCarousel({
+  startPosition: 12,
+  items:4,
+  loop:false,
+  margin:10,
+  autoplay:false,
+  nav: false,
+  dots: false,
+  onInitialized: function (e) {
+    var thumbnailCurrentItem =  $(e.target).find(thumbnailItemClass).eq(this._current);
+    thumbnailCurrentItem.addClass('synced');
+  },
+})
+.on('click', thumbnailItemClass, function(e) {
+    e.preventDefault();
+    var duration = 300;
+    var itemIndex =  $(e.target).parents(thumbnailItemClass).index();
+    sync1.trigger('to.owl.carousel',[itemIndex, duration, true]);
+}).on("changed.owl.carousel", function (el) {
+  var number = el.item.index;
+  $owl_slider = sync1.data('owl.carousel');
+  $owl_slider.to(number, 100, true);
+});
+
+
+// ============img-preview
+// ============// ============// ============// ============// ============
+
+jQuery(document).ready(function () {
+  ImgUpload();
+});
+
+function ImgUpload() {
+  var imgWrap = "";
+  var imgArray = [];
+
+  $('.upload__inputfile').each(function () {
+    $(this).on('change', function (e) {
+      imgWrap = $(this).closest('.upload__box').find('.upload__img-wrap');
+      var maxLength = $(this).attr('data-max_length');
+
+      var files = e.target.files;
+      var filesArr = Array.prototype.slice.call(files);
+      var iterator = 0;
+      filesArr.forEach(function (f, index) {
+
+        if (!f.type.match('image.*')) {
+          return;
+        }
+
+        if (imgArray.length > maxLength) {
+          return false
+        } else {
+          var len = 0;
+          for (var i = 0; i < imgArray.length; i++) {
+            if (imgArray[i] !== undefined) {
+              len++;
+            }
+          }
+          if (len > maxLength) {
+            return false;
+          } else {
+            imgArray.push(f);
+
+            var reader = new FileReader();
+            reader.onload = function (e) {
+              var html = "<div class='upload__img-box'><div style='background-image: url(" + e.target.result + ")' data-number='" + $(".upload__img-close").length + "' data-file='" + f.name + "' class='img-bg'><div class='upload__img-close'></div></div></div>";
+              imgWrap.append(html);
+              iterator++;
+            }
+            reader.readAsDataURL(f);
+          }
+        }
+      });
+    });
+  });
+
+  $('body').on('click', ".upload__img-close", function (e) {
+    var file = $(this).parent().data("file");
+    for (var i = 0; i < imgArray.length; i++) {
+      if (imgArray[i].name === file) {
+        imgArray.splice(i, 1);
+        break;
+      }
+    }
+    $(this).parent().parent().remove();
+  });
+}
